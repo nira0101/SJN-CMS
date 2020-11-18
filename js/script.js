a58906e1accffc47b8313feec77b1cee8bb03c7b
nira0101 <nira0101@stud.kea.dk>
2020-11-18 21:35:28 +0100

    ADD SUBPAGES,EDITED AND MADE SOME CHANES IN BUTTONS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-fetch("http://nirajan.dk/WPCMS/wp-json/wp/v2/description/?_embed")
+
+window.addEventListener("DOMContentLoaded",getData);
+
+const link = "http://nirajan.dk/WPCMS/wp-json/wp/v2/description/?_embed";
+
+function getData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log("URLSearchParams " + window.location);
+    const the_pension_id = urlParams.get("description_id");
+    console.log(the_pension_id);
+    if(the_pension_id) {
+        fetch("http://nirajan.dk/WPCMS/wp-json/wp/v2/description/" + the_pension_id + "?_embed")
+        .then(initial => initial.json())
+.then(showPension)
+    }
+     else{
+         fetch(link)
 .then(initial => initial.json())
 .then(callBack)
+     }
+
+
+}
 
 function callBack(data) {
     console.log(data)
@@ -15,11 +35,21 @@ function showPension(singlePension){
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
 
+
+
+
     const mainEl = document.querySelector(".m2");
     copy.querySelector(".pension h3").textContent = singlePension.title.rendered;
+     const divPensionDesc = copy.querySelector("#pension-description");
+    if(divPensionDesc) {
+        divPensionDesc.innerHTML = singlePension.content.rendered;
+    }
 
     const a = copy.querySelector(".pbut");
-    a.href += description.id;
+    if(a){
+         a.href += singlePension.id;
+    }
+
 
     copy.querySelector(".pimage").src = img_url;
     mainEl.appendChild(copy);
